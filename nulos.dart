35410ef380e18312ce356fbe1159270c01e8782b
Héctor Selley <hector.selley@gmail.com>
2023-08-29 23:45:55 -0600

    Mas ejemplos acerca de nulos

--- nulos.dart
+++ nulos.dart
@@ -21,6 +21,19 @@ void main() {
   int? y = null;
   print(y);
 
+  int? age;
+  double? height;
+  String? message;
+
+  print(age);
+  print(height);
+  print(message);
+  // print(message.length);
+
+  String? name;
+  name = "Bradley";
+  print(name);
+
   /// Función que determina si un entero es positivo, tolerante a nulos
   bool isPositive(int? anInteger) {
     if (anInteger == null) {
@@ -29,11 +42,60 @@ void main() {
     return !anInteger.isNegative;
   }
 
-  print(isPositive(null));
   int v = 1;
   print(isPositive(v));
 
-  String? name;
-  name = 'Bradley';
-  print(name.length);
+  var lista = [14, 0, -21, null];
+  lista.forEach((element) {
+    print(element);
+    print(isPositive(element));
+  });
+
+  /* Operador if-null */
+  // Ejemplo 1:
+  String? message2;
+  final text = message2 ?? 'Error';
+  print(text);
+
+  // El código del ejemplo 1 es equivalente a:
+  double? fontSize;
+  // Se puede escribir así:
+  fontSize = fontSize ?? 20.0;
+  // O su contracción:
+  fontSize ??= 20.0;
+  print(fontSize);
+
+  /* Operador de acceeso */
+  int? age2;
+  print(age2?.isNegative);
+  print(age2?.toDouble());
+
+  /* Operador afirmación ! */
+  String? myNullableString = 'b';
+  String nonNullableString = myNullableString!;
+  // Esta advertencia ocurre porque la variable no es nula
+  print(nonNullableString);
+
+  // Ejemplo:
+  bool? isBeautiful(String? item) {
+    if (item == 'flower') {
+      return true;
+    } else if (item == 'garbage') {
+      return false;
+    }
+    return null;
+  }
+
+  // El operador afirmación indica que la variable no es nula, por lo que
+  // la asignación es válida.
+  bool flowerIsBeautiful = isBeautiful('flower')!;
+
+  /// Esta alternativa lleva a cabo un _cast-down_ al tipo de dato:
+  /// bool? -> bool
+  bool flowerIsBeautiful2 = isBeautiful('flower') as bool;
+  print(flowerIsBeautiful2);
+
+  // Una opción más segura es la siguiente:
+  bool flowerIsBeautiful3 = isBeautiful('flower') ?? true;
+  print(flowerIsBeautiful3);
 }
